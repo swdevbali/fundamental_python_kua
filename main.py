486a91b68d9165c734ff90b77efbfc80a4f45d05
Eko <swdev.bali@gmail.com>
2020-03-07 11:22:10 +0700

    tahap 2

--- main.py
+++ main.py
@@ -20,3 +20,37 @@ usia = 5
 for u in range(1, usia + 1):
     print('Ulang tahun ke', u)
 
+hari = [] #tipe data array atau list
+hari.append('Minggu')
+hari.append('Senin')
+hari.append('Selasa')
+hari.append('Rabu')
+hari.append('Kamis')
+hari.append('Jumat')
+hari.append('Sabtu')
+hari.append('Sabgu')
+
+print(hari[0])
+print('Panjang hari', len(hari))
+print(hari[len(hari) - 1])
+
+for h in hari:
+    print(h)
+
+luas_alas = 40
+tinggi = 3
+luas_segitiga = luas_alas * tinggi / 2
+print(luas_segitiga)
+
+luas_alas = 60
+tinggi = 3
+luas_segitiga = luas_alas * tinggi / 2
+print(luas_segitiga)
+
+def hitung_luas_segitiga(luas_alas, tinggi):
+    return luas_alas * tinggi / 2
+
+print(hitung_luas_segitiga(90, 3))
+print(hitung_luas_segitiga(45, 7))
+
+# NEXT: CLASS, PACKAGE. Googling yaks!
