1b5d44e7958abc5a55f9feb55f208cccdd0a468e
Eko Suprapto Wibowo <swdev.bali@gmail.com>
2020-03-07 11:05:08 +0700

    tahap 1

---
+++ main.py
@@ -0,0 +1,22 @@
+print("Hello World")
+nama = 'Eko'
+print(nama)
+usia = 60
+print(usia)
+
+#usia = 'empat puluh'
+#print(usia)
+
+if usia <= 40:
+    print('Msih muda')
+    print('Semangat kerja')
+elif usia > 40 and usia <= 50:
+    print('Tua')
+    print('Ibadah')
+else:
+    print('Tobat')
+
+usia = 5
+for u in range(1, usia + 1):
+    print('Ulang tahun ke', u)
+
